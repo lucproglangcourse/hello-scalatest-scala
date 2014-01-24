fe6b63f452ba963aa66242ece7963235a0795f3e
Konstantin Laufer <laufer@cs.luc.edu>
2014-01-23 23:48:37 -0600

    converted to sbt

--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -1,17 +1,13 @@
 package hellotest
 
 // example straight from scalatest.org 
-// but slightly modified to run as a JUnit test in Eclipse
 
 import scala.collection.mutable.Stack
 
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
 import org.scalatest.Suite
 import org.scalatest.matchers.MustMatchers
 
-@RunWith(classOf[JUnitRunner])
 class StackSpec extends FlatSpec with MustMatchers with Suite {
 
   "A Stack" should "pop values in last-in-first-out order" in {
@@ -24,6 +20,6 @@ class StackSpec extends FlatSpec with MustMatchers with Suite {
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
     val emptyStack = new Stack[String]
-    evaluating { emptyStack.pop() } must produce [NoSuchElementException]
+    evaluating { emptyStack.pop() } must produce[NoSuchElementException]
   }
 }
