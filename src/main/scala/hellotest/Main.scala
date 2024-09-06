7bf0719176f4e4dac2e87b76838a1c9ed468a770
Konstantin Läufer <laufer@cs.luc.edu>
2024-09-06 12:47:34 -0500

    converted tests to Scala 3 syntax without braces
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -1,5 +1,9 @@
 package hellotest
 
 object Main:
-  def main(args: Array[String]) = println("hello scalatest")
+
+  def main(args: Array[String]) = 
+    println("Hello scalatest!")
+    println(s"Today's date is ${java.time.LocalDate.now}.")
+
 end Main
--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -10,16 +10,16 @@ import org.scalatest.matchers.must.Matchers.*
 
 class StackSpec extends AnyFlatSpec with Suite:
 
-  "A Stack" should "pop values in last-in-first-out order" in {
+  "A Stack" should "pop values in last-in-first-out order" in:
     val stack = Stack.empty[Int]
     stack.push(1)
     stack.push(2)
     stack.pop() must equal(2)
     stack.pop() must equal(1)
-  }
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
+  it should "throw NoSuchElementException if an empty stack is popped" in:
     val emptyStack = Stack.empty[Int]
-    an[NoSuchElementException] must be thrownBy { emptyStack.pop() }
-  }
+    an[NoSuchElementException] must be thrownBy:
+      emptyStack.pop()
+
 end StackSpec
